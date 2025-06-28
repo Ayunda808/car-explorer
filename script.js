bf29ec6765dcdfb070e33e5984612a4480caefd3
Ryan ayunda <ryan.ayunda@student.moringaschool.com>
2025-06-28 09:20:51 +0300

    Update fetch to use live API

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let allCars = [];
 let favoriteCars = [];
 
 // ======= Fetch Data from json-server =======
-fetch('http://localhost:3000/cars')
+fetch('https://ayunda808.github.io/car-explorer/')
   .then(response => response.json())
   .then(data => {
     allCars = data;
@@ -32,7 +32,7 @@ function displayCars(cars) {
       <p><strong>Fuel:</strong> ${car.fuel_type}</p>
       <p><strong>Transmission:</strong> ${car.transmission}</p>
       <p><strong>Body Type:</strong> ${car.body_type}</p>
-      <button class="favorite-btn" data-id="${car.id}">❤️ Favorite</button>
+      <button class="favorite-btn" data-id="${car.id}"> Favorite</button>
     `;
 
     const favBtn = card.querySelector('.favorite-btn');
